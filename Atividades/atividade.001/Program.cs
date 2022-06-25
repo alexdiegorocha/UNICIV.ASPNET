d951a04b05f3ccfa42c027e70d60d725891f8990
Alex Rocha <diego.rocha@kralen.com.br>
2022-06-25 16:51:03 -0300

    atv/001 - Implementando o primeiro projeto: Olá Mundo!

---
+++ Atividades/atividade.001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace atividade._001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
