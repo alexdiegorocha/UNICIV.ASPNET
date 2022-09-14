56a27d35cbadffcd92ba5e10fa190e5d203bc8ad
Marcos Katayama <marcos.katayama@outlook.com.br>
2022-09-13 22:52:26 -0300

    Projeto Gestão Escolar

---
+++ Atividades/atividade.005/gestaoescolar/src/app/core/services/service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { first } from 'rxjs';
+
+import { API_PATH } from '../../../environments/environment';
+
+export abstract class Service<T> {
+
+  constructor(protected httpClient: HttpClient, protected route: string) {
+
+  }
+  get() {
+    return this.httpClient.get<T[]>(`${API_PATH}api/${this.route}`)
+      .pipe(
+        first()
+      );
+  }
+
+  getById(id: number) {
+    return this.httpClient.get<T>(`${API_PATH}api/${this.route}/${id}`)
+    .pipe(
+      first()
+    );
+  }
+
+  post(record: T) {
+    console.log(`${API_PATH}api/${this.route}`, record);
+    return this.httpClient.post<T>(`${API_PATH}api/${this.route}`, record).pipe(
+      first()
+      );
+  }
+
+  put(id: number, record: T) {
+    return this.httpClient.put<T>(`${API_PATH}api/${this.route}/${id}`, record);
+  }
+
+  delete(id: number) {
+    return this.httpClient.delete<T>(`${API_PATH}api/${this.route}/${id}`);
+  }
+}
---
+++ Atividades/atividade.005/gestaoescolar/src/app/modules/cadastros/aluno/aluno-list/aluno-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Aluno } from 'src/app/core/models/aluno.model';
+import { AlunoService } from 'src/app/core/services/aluno.service';
+
+@Component({
+  selector: 'app-aluno-list',
+  templateUrl: './aluno-list.component.html',
+  styleUrls: ['./aluno-list.component.sass'],
+})
+export class AlunoListComponent implements OnInit {
+  model: Aluno ={};
+  listaAluno: Observable<Aluno[]>;
+  constructor(private alunoService: AlunoService, private router: Router) {
+    this.listaAluno = this.alunoService.get();
+   }
+
+  ngOnInit(): void {
+  }
+
+  cmdRemover(item: Aluno) {
+    var id: number;
+    id = Number(item.id);
+    this.alunoService.delete(id).subscribe(() => {});
+    this.load();
+  }
+
+  cmdEditar(item: Aluno){
+    var id: number;
+    id = Number(item.id);
+    this.router.navigateByUrl('cadastros/aluno/'+id+'/editar');
+  }
+
+  load() {
+    location.reload();
+  }
+}
